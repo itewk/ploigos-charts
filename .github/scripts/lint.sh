7e97f861e628f55953220f0e891c93179b8e7a0b
Ian Tewksbury <itewk@redhat.com>
2021-06-22 15:23:04 -0400

    WIP - .github workflows - CHARTS_DIR env variable

--- .github/scripts/lint.sh
+++ .github/scripts/lint.sh
@@ -4,6 +4,6 @@
 #   --validate-chart-schema=false due to https://github.com/helm/chart-testing/pull/300
 
 ct lint \
-    --chart-dirs charts/ploigos-workflow/ \
+    --chart-dirs ${CHARTS_DIR} \
     --all \
     --validate-maintainers=false
